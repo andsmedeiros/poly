38b22fb21aeea1299a50f239afa02fb71eacddcb
André Medeiros <andsmedeiros@gmail.com>
2022-12-11 18:05:14 -0300

    Refactoring
    * Removes unused header
    * Uses std namespace in std::nullptr_t
    * Adds constexpr helpers to tell aggregates and scalas apart

--- include/poly/primitives.hpp
+++ include/poly/primitives.hpp
@@ -3,7 +3,6 @@
 
 #include <cstddef>
 #include <cstdint>
-#include <cwchar>
 #include <map>
 #include <memory>
 #include <string>
@@ -15,7 +14,7 @@ namespace poly {
 
 class data;
 
-using null = nullptr_t;
+using null = std::nullptr_t;
 using integer = std::int64_t;
 using real = double;
 using boolean = bool;
@@ -44,8 +43,26 @@ using array_ptr = std::shared_ptr<array>;
 struct index_type : public std::variant<object::index_type, array::index_type> {
     using variant::variant;
 
-    index_type(int index) : variant { static_cast<array::index_type>(index) } {  }
+    index_type(int index) : 
+        variant { static_cast<array::index_type>(index) } {  }
 };
+
+template<class T>
+inline constexpr bool is_aggregate() noexcept {
+    return
+        std::is_same_v<T, string> ||
+        std::is_same_v<T, object> ||
+        std::is_same_v<T, array>;
+}
+
+template<class T>
+inline constexpr bool is_scalar() noexcept {
+    return 
+        std::is_same_v<T, null>     ||
+        std::is_same_v<T, integer>  ||
+        std::is_same_v<T, real>     ||
+        std::is_same_v<T, boolean>;
+}
 
 } /* namespace poly */
 
